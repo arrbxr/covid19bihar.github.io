0247c04b01a2f28aa98aef8aa5f3319dd492ab61
arrbxr <arr.bxr@gmail.com>
2020-05-17 14:03:58 +0530

    Updates

---
+++ precache-manifest.00bb098a40004fef6ddaf9a546c586d4.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63895aec1c32bd2bdfb888729a2081ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17a427319aeea93b273f",
+    "url": "/static/css/2.c4723202.chunk.css"
+  },
+  {
+    "revision": "91b370ee588a3b7cde68",
+    "url": "/static/css/main.1abf49e5.chunk.css"
+  },
+  {
+    "revision": "17a427319aeea93b273f",
+    "url": "/static/js/2.90b62696.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/static/js/2.90b62696.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91b370ee588a3b7cde68",
+    "url": "/static/js/main.dab49c4e.chunk.js"
+  },
+  {
+    "revision": "1802ec32458b04107b4e",
+    "url": "/static/js/runtime-main.559afb7a.js"
+  },
+  {
+    "revision": "8f95c3462abc698639bbb3fa453ed460",
+    "url": "/static/media/advice1.8f95c346.png"
+  },
+  {
+    "revision": "7bca717fe28ec4e73a2ec84448f6f994",
+    "url": "/static/media/advice2.7bca717f.png"
+  },
+  {
+    "revision": "3fd044dc540c9eb96c997efa266137c7",
+    "url": "/static/media/advice3.3fd044dc.png"
+  },
+  {
+    "revision": "00f14d54f46526af68d30a849b21d27a",
+    "url": "/static/media/advice4.00f14d54.png"
+  },
+  {
+    "revision": "953ec9749362895382419d5adce43992",
+    "url": "/static/media/avoidface.953ec974.jpg"
+  },
+  {
+    "revision": "df0652a2e11c5a549405c42d83f0cb11",
+    "url": "/static/media/breath.df0652a2.png"
+  },
+  {
+    "revision": "6633d7014aa326c9a474e0fb56fd487a",
+    "url": "/static/media/cough.6633d701.png"
+  },
+  {
+    "revision": "a9ec9dedeab0a20c0ba7c8ca91be249b",
+    "url": "/static/media/fever.a9ec9ded.png"
+  },
+  {
+    "revision": "4e7b8f828334a7a448ebb0b9d87d2de6",
+    "url": "/static/media/handWash.4e7b8f82.jpg"
+  },
+  {
+    "revision": "803bbb3dacfdb4db76f35579ffc2bf0c",
+    "url": "/static/media/keepdistance.803bbb3d.jpg"
+  },
+  {
+    "revision": "00c506953d48b8c69a7ecbf8ddb92871",
+    "url": "/static/media/smokee.00c50695.jpg"
+  }
+]);
